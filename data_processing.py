31e375cbb78d39708fe8617938b275543ee69226
iopenattheclose <prupro69@gmail.com>
2024-12-05 22:59:25 +0530

    data preprocessing

---
+++ data_processing.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from IPython.display import Image
+from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, accuracy_score
+import re
+import matplotlib.pyplot as plt
+import string
+
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+
+
+def ingestData():
+    df = pd.read_csv("/Users/prupro/Desktop/Github/NewsClassRNN/artifacts/bbc-news-data.csv", sep="\t")
+    df = df[((~df.title.isnull()) & (~df.content.isnull()))].reset_index(drop=True)
+    print(f'Data shape and size is : {df.shape} and {df.shape[0]}')
+    return df
+
+def data_cleaning(text):
+
+    # Lower the words in the sentence
+    cleaned = text.lower()
+    # Replace the full stop with a full stop and space
+    cleaned = cleaned.replace(".", ". ")
+    # Remove the stop words : optional pre-processing step
+    tokens = [word for word in cleaned.split() if not word in stop_words]
+    # Remove the punctuations
+    tokens = [tok.translate(str.maketrans(' ', ' ', string.punctuation)) for tok in tokens]
+    # Joining the tokens back to form the sentence
+    cleaned = " ".join(tokens)
+    # Remove any extra spaces
+    cleaned = cleaned.strip()
+
+    return cleaned
+
+
+if __name__ == "__main__":
+    df = ingestData()
+    for index, data in tqdm(df.iterrows(), total=df.shape[0]):
+        df.loc[index, 'title'] = data_cleaning(data['title'])
