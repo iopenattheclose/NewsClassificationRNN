5663e65e67370d636f989318103351e20716457f
iopenattheclose <prupro69@gmail.com>
2024-12-06 22:42:01 +0530

    model training

--- src/data_processing.py
+++ src/data_processing.py
@@ -7,6 +7,8 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 import re
 import matplotlib.pyplot as plt
 import string
+from sklearn.model_selection import train_test_split
+
 
 import nltk
 nltk.download('stopwords')
@@ -44,8 +46,31 @@ def set_max_seq_length():
     print(f"Maximum sequence length: {max_sentence_len}")
     print(f"Total classes: {total_classes}")
 
+def cleaned_data():
+    tqdm.pandas()
+    # Apply the data_cleaning function to the 'title' column
+    df['title'] = df['title'].progress_apply(data_cleaning)
+    print(df.head())
+    return df
+
+def splitData():
+    np.random.seed(100)
+    train_X, test_X, train_Y, test_Y = train_test_split(df['title'],
+                                                        df['category'],
+                                                        test_size=0.2,
+                                                        random_state=100)
+    train_X = train_X.reset_index(drop=True)
+    test_X = test_X.reset_index(drop=True)
+    train_Y = train_Y.reset_index(drop=True)
+    test_Y = test_Y.reset_index(drop=True)
+    print(train_X.shape, train_Y.shape, test_X.shape, test_Y.shape)
+    return train_X,test_X,train_Y,test_Y
+
+def encodeLabels():
+    train_Y = pd.get_dummies(train_Y).values
+    test_Y = pd.get_dummies(test_Y).values
+
+
 if __name__ == "__main__":
     df = ingestData()
-    for index, data in tqdm(df.iterrows(), total=df.shape[0]):
-        df.loc[index, 'title'] = data_cleaning(data['title'])
     set_max_seq_length()
